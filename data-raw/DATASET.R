eef05194a84a3d4dcb7cd089c1574fa0ce01c05d
lilydiao <ldiao@smith.edu>
2022-11-09 08:25:36 -0500

    creating the anime.rda file

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,7 @@
 ## code to prepare `DATASET` dataset goes here
+library(tidyverse)
+library(janitor)
+anime.csv<-read_csv("data-raw/Anime.csv") %>%
+  clean_names()
 
 usethis::use_data(DATASET, overwrite = TRUE)
