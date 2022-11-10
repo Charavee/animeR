e3215e98b8c8d1d9f3963db649ccc6d89b13b0f9
Michiru0206 <mnozawa@smith.edu>
2022-11-10 10:10:25 -0500

    solved issue

--- R/process_datasets.R
+++ R/process_datasets.R
@@ -1,17 +1,14 @@
 #' Anime data set
 #'
-#' A list of anime
+#' @format A data frame with 21460 rows and 28 columns
 #'
-#' @format ## `anime`
-#' A data frame with 21460 rows and 28 columns:
 #' \describe{
-#'   \item{id}
-#'   \item{title}
-#'   \item{synonyms}
-#'   \item{synopsis}
-#'   \item{japanese}
-#'   \item{english}{type}
-#'   ...
+#' \item{id}{abcd}
+#' \item{title}{abcd}
+#' \item{synonyms}{abcd}
+#' \item{synopsis}{abcd}
+#' \item{japanese}{abcd}
+#' \item{english}{type}
 #' }
 #' @source <https://www.kaggle.com/datasets/CooperUnion/anime-recommendations-database>
 "anime"
