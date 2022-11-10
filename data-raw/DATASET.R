14c20c56428591c628590137723c6f0f3260d4ea
Michiru0206 <mnozawa@smith.edu>
2022-11-10 10:40:59 -0500

    change the name

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,7 +1,7 @@
 ## code to prepare `DATASET` dataset goes here
 library(tidyverse)
 library(janitor)
-anime<-read_csv("data-raw/Anime.csv") %>%
+anime<-read_csv("data-raw/anime.csv") %>%
   clean_names()
 
 usethis::use_data(anime, overwrite = TRUE)
