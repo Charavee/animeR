15b8f0a923e978915e6ef7266c7c652c0a4ecbe2
Michiru0206 <mnozawa@smith.edu>
2022-11-21 11:04:37 -0500

    Create a new function, anime_rec()

--- R/anime_rec.R
+++ R/anime_rec.R
@@ -1,4 +1,43 @@
-anime_rec <- function(user_genre, user_year) {
-  anime %>%
-    pivot_longer(cols = c("genres"))
+#' Find Top 5 Popular Anime
+#'
+#' @description This function helps you to find the top 5 most popular anime in its certain genre and source. There are 22 unique genres and 17 unique sources to choose from.
+#'
+#' @param user_genre : A character vector of genre of the anime. Users can pick one genre out of the list of 22 genres we provide.
+#'
+#' @param user_source: A character vector of source of the anime. Users can also pick one source out of the list of 17 sources.
+#'
+#' @return A tibble of top five most popular anime with their titles, synopsis, and popularity.
+#'
+#' @examples
+#'
+#' library(animeR)
+#'
+#' # Top five most popular action anime that are based on manga
+#' anime_rec("Action", "Manga")
+#'
+#' @import dplyr
+#' @importFrom magrittr "%>%"
+#' @importFrom stringr "str_detect"
+#' @export
+
+
+anime_rec <- function(user_genre, user_source) {
+  # split genres and create new rows
+  anime_genres <- anime %>%
+    mutate(genres = strsplit(as.character(genres), ", |, ")) %>%
+    unnest(genres)
+
+  # Show top 5 most popular anime based on the given genre and source
+  if (user_genre %in% anime_genres$genres & user_source %in% anime_genres$source) {
+    user_anime_rec <- subset(anime_genres, select = c("title", "synopsis", "popularity", "genres", "source")) %>%
+      dplyr::filter(genres == user_genre & source == user_source)
+
+    user_anime_rec[order(user_anime_rec$popularity),] %>%
+      dplyr::select(-c("genres", "source")) %>%
+      head(n = 5)
+
+  }else{
+    # Print error message
+    message("ERROR: At least one of your input is not in the dataset. Please check the list of genres and source and try again.")
+  }
 }
