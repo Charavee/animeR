3891e47aacf12a7176cf325d066f9ce9b35abbe7
lilydiao <ldiao@smith.edu>
2022-12-08 22:17:07 -0500

    updating ideas of a final function

--- R/predict_popularity.R
+++ R/predict_popularity.R
@@ -2,3 +2,11 @@
 
 model1 <- glm(score ~ ., data = anime, family = "binomial")
 summary.glm(model1)
+
+duration distirbution
+time aired distribution
+number of episodes distribution
+most popular producers
+most populor licensors
+most common genres
+top five themes
